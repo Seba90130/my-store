af229d106e38b5b707e3f6d404540b759fb3a397
Sebastian Suarez Solano <seba90130@rd.local>
2023-09-17 15:35:53 -0400

    Proyecto API

---
+++ routes/products.router.js
@@ -0,0 +1,75 @@
+const express = require('express');
+
+const ProductsServices = require('./../services/product.service');
+const validatorHandler = require('./../middlewares/validator.handler');
+const {
+  createProductSchema,
+  updateProductSchema,
+  getProductSchema,
+} = require('./../schemas/product.schema');
+
+const router = express.Router();
+const service = new ProductsServices();
+
+//READ ALL
+router.get('/', async (req, res) => {
+  const products = await service.find();
+  res.json(products);
+});
+
+//FILTER
+router.get('/filter', async (req, res) => {
+  res.send('Yo soy un filter');
+});
+
+//READ ONE
+router.get(
+  '/:id',
+  validatorHandler(getProductSchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const product = await service.findOne(id);
+      res.json(product);
+    } catch (error) {
+      next(error);
+    }
+  },
+);
+
+//CREATE NEW PRODUCT
+router.post(
+  '/',
+
+  validatorHandler(createProductSchema, 'body'),
+  async (req, res) => {
+    const body = req.body;
+    const newProduct = await service.create(body);
+    res.status(201).json(newProduct);
+  },
+);
+
+//EDIT A PRODUCT
+router.patch(
+  '/:id',
+  validatorHandler(getProductSchema, 'params'),
+  validatorHandler(updateProductSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const body = req.body;
+      const product = await service.update(id, body);
+      res.json(product);
+    } catch (error) {
+      next(error);
+    }
+  },
+);
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  const rta = await service.delete(id);
+  res.json(rta);
+});
+
+module.exports = router;
